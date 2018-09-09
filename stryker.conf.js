0968ba432387f61b078ac5db69f28d6236ad8353
CheshireSwift <github@swifteagle.co.uk>
2018-09-09 17:02:27 +0100

    Stricter CI build

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,7 +2,6 @@ module.exports = function(config) {
   config.set({
     testRunner: "jest",
     mutator: "typescript",
-    // transpilers: ["babel", "typescript"],
     transpilers: [],
     reporters: ["clear-text", "progress"],
     packageManager: "yarn",
@@ -10,6 +9,6 @@ module.exports = function(config) {
     tsconfigFile: "tsconfig.json",
     mutate: ["src/**/*.ts", "!src/**/*.@(test|helper|d).ts", "!src/main.ts"],
     babelrcFile: ".babelrc",
-    thresholds: { high: 80, low: 60, break: 90 }
+    thresholds: { high: 100, low: 95, break: 90 }
   });
 };
